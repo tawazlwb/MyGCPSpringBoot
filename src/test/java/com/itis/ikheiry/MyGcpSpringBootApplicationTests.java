a98499ef622493f66d6080a7be9cbe9d012f916c
tawazlwb <tawazlwb@gmail.com>
2017-11-14 17:48:11 +0100

    First commit

---
+++ src/test/java/com/itis/ikheiry/MyGcpSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itis.ikheiry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyGcpSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
